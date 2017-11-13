ef4bca593644ff73bdb7a4c9f9e889959e42e8b6
Driven Coder <drivencoder@gmail.com>
2017-11-13 13:06:20 -0500

    First Commit - Empty Window

---
+++ Ponvader/Ponvader/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+void main()
+{
+	sf::RenderWindow gameWindow(sf::VideoMode(640, 480), "Ponvader");
+
+	while (gameWindow.isOpen())
+	{
+		sf::Event event;
+
+		while (gameWindow.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				gameWindow.close();
+		}
+
+		gameWindow.clear();
+		gameWindow.display();
+	}
+}
